8431093b188097835e78604e3434b66ace2a7c2e
Simone Fantini <mones88@gmail.com>
2015-02-09 00:20:50 +0100

    Update template and helpers for angular resource

--- AngularMVC.T4Templates/Helpers/AngularResources.cs
+++ AngularMVC.T4Templates/Helpers/AngularResources.cs
@@ -4,9 +4,40 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace AngularMVC.T4Templates.Helpers
 {
+    public static class AngularResourcesHelpers
+    {
+        private static string[] ValidActionsMethodNames = { "Get", "Post", "Put", "Delete" };
+
+        public static IEnumerable<MethodInfo> GetValidActionsFromController(Type controllerType) 
+        {
+            var methods = controllerType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            return methods.Where(mi => IsValidActionName(mi.Name) || IsMethodAdornedWithValidAttribute(mi.GetCustomAttributes()));
+        }
+
+        private static bool IsValidActionName(string name)
+        {
+            return ValidActionsMethodNames.Contains(name);
+        }
+
+        private static bool IsMethodAdornedWithValidAttribute(IEnumerable<Attribute> attributes)
+        {
+            if (attributes == null || !attributes.Any())
+                return false;
+
+            return attributes.Any(attr => {
+                var attrType = attr.GetType();
+                return attrType == typeof(HttpGetAttribute) ||
+                    attrType == typeof(HttpPostAttribute) ||
+                    attrType == typeof(HttpPutAttribute) ||
+                    attrType == typeof(HttpDeleteAttribute);
+            });
+        }
+    }
+
     public class WebApiControllerMethodsInfos
     {
         public string MethodName { get; set; }
@@ -22,7 +53,7 @@ namespace AngularMVC.T4Templates.Helpers
 
         public WebApiControllerMethodsInfos(MethodInfo mi)
         {
-            this.HttpMethod = HttpMethodFromMethodName(mi.Name);        
+            this.HttpMethod = HttpMethodFromMethodName(mi.Name);
         }
 
         private static string HttpMethodFromMethodName(string name)
